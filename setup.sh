29e32a1c8ab060a9ef4482813db91cc10fc4177c
carzpurzkey <carzpurzkey@live.com>
2026-09-24 10:25:45 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ sudo apt-get update
 sudo apt-get -y install --no-install-recommends apt-utils dialog 2>&1
 sudo apt-get install -y \
   xfonts-utils \
+  fontconfig \
   curl \
   git \
   gnupg2 \
