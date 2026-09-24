6f7b9c10a42ed59325fb678f7f25f49ca908ce3f
carzpurzkey <carzpurzkey@live.com>
2026-09-24 09:53:55 +0800

    Use sudo for changing default shell to zsh

--- setup.sh
+++ setup.sh
@@ -49,7 +49,7 @@ then
     zsh
 
     #cp -f ~/dotfiles/.zshrc ~/.zshrc
-    chsh -s /usr/bin/zsh $USERNAME
+    sudo chsh -s /usr/bin/zsh $USERNAME
     wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
